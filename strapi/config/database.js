49dedb0cb491f93ab99e95bef581f5506cab0c5d
LaureLavie <laure.lavie@gmail.com>
2026-07-16 16:10:33 +0200

    strapi

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -1,9 +1,9 @@
-module.exports = ({ env }: { env: (key: string, defaultValue?: any) => any }) => ({
+module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
       host: env('DATABASE_HOST', 'postgres'),
-      port: Number(env('DATABASE_PORT', 5432)),
+      port: env('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'fnp_strapi'),
       user: env('DATABASE_USERNAME', 'fnp_admin'),
       password: env('DATABASE_PASSWORD', 'change_me_strong_password'),
