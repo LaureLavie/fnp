755b24b6ae70feaedcc200e36c8386b6930ac867
LaureLavie <laure.lavie@gmail.com>
2026-07-21 14:48:39 +0200

    database.ts strapi env.pool

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -3,9 +3,12 @@ export default ({ env }: { env: any }) => ({
     client: env('DATABASE_CLIENT', 'postgres'),
     connection: {
       connectionString: env('DATABASE_URL'),
-      ssl: env.bool('DATABASE_SSL', false),
+      ssl: env('DATABASE_SSL', 'false') === 'true',
     },
     useNullAsDefault: true,
-    pool: { min: 0, max: 10 },
+    pool: {
+      min: parseInt(env('DATABASE_POOL_MIN', '0'), 10),
+      max: parseInt(env('DATABASE_POOL_MAX', '10'), 10)
+    },
   },
 });
